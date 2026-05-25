0a95c48f9306778b8b8e586f5f43d0686f960096
wane-bs <01134wane@gmail.com>
2026-05-25 11:20:22 +0700

    Updates

---
+++ _next/static/8KVUSQWs-0mlYMgARawyv/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002F[regular]","\u002Fcategories","\u002Fcategories\u002F[category]","\u002Fcover-letter","\u002Fpage\u002F[slug]","\u002Fposts","\u002Fposts\u002F[single]","\u002Fprojects\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
